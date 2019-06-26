c7a643c50cc6b723923bbd816a40278f1275dd03
Elvis Aguero <elvisavfc65@gmail.com>
2019-06-26 14:27:33 -0400

    Agregado algunos ejercicios sobre listas

--- ejercicios(listas).py
+++ ejercicios(listas).py
@@ -26,3 +26,46 @@ x = [1, 3, True, [4,3], 6, "String", 6, 4, -1]
  2) Esta el valor 0 en esta lista?
  3) Imprima la posicion del valor mas cercano a 100 que haya en la lista
  
+ ------------------------------------
+ Ejercicio 3:
+ 
+ Defina una lista con 10 elementos (sin ceros)
+ 
+ Guarde en la variable "prod_list" el producto de todos los elementos
+ de la lista
+ 
+ ------------------------------------
+ Ejercicio 4:
+ 
+ Defina una lista al comienzo del script, unicamente con strings.
+ 
+ El programa debe imprimir la cantidad de elementos de la lista que
+ tienen dos o mas caracteres
+ 
+ ----------------------------------------
+ Ejercicio 5:
+ 
+ Defina una lista cualquiera al comienzo de un script
+ 
+ El programa debe imprimir la misma lista, pero eliminando valores repetidos.
+ 
+ -----------------------------------------
+ Ejercicio 6:
+ 
+ Para este ejercicio, se necesitaran dos listas, definalas al comienzo de un script
+ El programa debe retornar un booleano. True en caso de que las listas compartan un 
+ elemento en comun, y False caso contrario
+ 
+ Ejemplo: si las listas fuesen [1, "Hola", False] y [[2,3], 1, True], el programa
+ debe retornar True, puesto que el elemento "1" esta en ambas listas
+ 
+ -----------------------------------------
+ Ejercicio 7:
+ 
+ Defina una lista al comienzo del script. La lista debe contener solo numeros
+ 
+ El programa debe imprimir otra lista, igual a la original pero todos los numeros
+ pares han sido eliminados
+ 
+ Ej: [1, 2, 3, 4, 5, 5, 6, 6] ---->  [1, 3, 5, 5]
+ '''
