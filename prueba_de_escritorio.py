42a3fca16813342de0a4a03d470f68f30a0dad27
Elvis Aguero <elvisavfc65@gmail.com>
2019-07-04 10:28:08 -0400

    Agregado ejercicios de prueba de escritorio

---
+++ prueba_de_escritorio.py
@@ -0,0 +1,29 @@
+x = 1
+y = 2
+print("Empecemos!")
+z = x * y + 1
+for i in range(1, z):
+    z = z + x * i - 3 * y
+if z > 10:
+    x = 0
+else:
+    y = 0
+
+#--------------------------------------
+
+'''
+a = [0]
+x = False
+b = 6 ** 3 % 2
+if ( a == b ):
+    print("Son iguales!")
+else:
+    x = not x
+    a.append(1)
+    a.extend([x, b])
+    
+'''
+
+
+
+
