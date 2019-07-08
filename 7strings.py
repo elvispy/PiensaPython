cd86eaa87e2ab698fabb6a18059aab2a30132d85
Elvis Aguero <elvisavfc65@gmail.com>
2019-07-08 13:32:53 -0400

    Agregado data.txt y 7srings para manipulacion de strings.

---
+++ 7strings.py
@@ -0,0 +1,33 @@
+'''Ahora vamos a aprender a manipular strings. Lo primero que debemos saber es 
+que un string puede ser visto como una lista de caracteres, en el sentido que tienen
+indexado
+'''
+
+b = 'Esto es una oracion'
+print(b[2])
+
+#for x in b:
+#    print(x)
+
+'''
+Tambien tienen otros metodos parecidos a las listas. Como hay muchos, lo mejor
+que se puede hacer es ver la documentacion. Mostraremos los mas utiles
+
+string.count(char) \\ va a contar la cantidad de veces que char aparece en string
+string.index(char) \\ Devuelve la posicion de char en string. Retorna error si
+char no se encuentra en string.
+string.upper() \\ conierte el string en mayusculas
+string.lower() \\ convierte el string en minusculas
+string.replace(viejo, nuevo) \\ reemplaza los viejos strings por los nuevos
+string.strip(char) \\ crea una lista donde el separador es el string char
+'''
+
+
+string = 'Veamos como nos va con este string.'
+
+#string.count('n')
+#string.index(' ')
+#string.upper()
+#string.lower()
+#string.replace("e", "E")
+#string.strip(" ")
---
+++ 8files.py
@@ -0,0 +1,35 @@
+''' Finalmente, debemos saber manejar datos externos. Por ejemplo, supongamos de
+nuevo que somos meteorologos, y registramos ciertas temperaturas a traves del dia.
+Entonces seria buena idea guardar los datos en un archivo externo.
+
+Una tentativa puede ser crear la lista y usar el metodo .append 
+'''
+a = [14, 17, 22] #hacer append en el shell
+
+'''
+Pero eso por si solo no funciona. Para eso, podemos abrir archivos externos
+
+
+with open('nombre_archivo', 'ar(+)w') as variable:
+    texto = variable.read()
+Entonces va a guardar un string con todo el texto en el. Un enter se
+guarda como '\n'.
+'''
+
+with open('data.txt', 'r') as datos:
+    texto = datos.read()
+print(texto)
+
+'''
+Existen varias funciones que nos ayudaran
+Ya vimos datos.read(), va a devolver un string con todos los caracteres restantes
+datos.readline() va a devolver solo la primera linea
+datos.tell() nos dira donde esta el cursor (no recibe argumentos)
+datos.seek() nos pondra el cursor en el lugar indigado (para leer)
+
+recuerden la diferencia entre los modos
+
+r = Solo para leer.
+r+ = Puede leer y sobreescribir, sin borrar lo que ya habia
+a = va a escribir todo al final, sin borrar lo que habia. Si no hay archivo, lo crea
+w = Va a sobreescribir todo lo que se agregue. Si no existe el archivo, se crea
